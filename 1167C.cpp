d76664b9ba2f1a5aac7310b07cf8e4dfb6e1088f
parul-jain5102 <63896325+parul-jain5102@users.noreply.github.com>
2021-06-18 21:25:38 +0530

    Create 1167C.cpp

---
+++ 1167C.cpp
@@ -0,0 +1,80 @@
+//Problem 1167C codeforces
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define MP make pair
+#define PB push back
+#define MOD 1000000007
+#define fi first #define se second
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<PII> VPII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSET;
+VI parent;
+VI ranks;
+int n,m;
+int Find(int a)
+{
+    if(parent[a]==-1)
+          return a;
+    parent[a]=Find(parent[a]);
+    return parent[a];
+}
+void union_set(int a,int b)
+{
+   int s1=Find(a);
+   int s2=Find(b); 
+    if(s1!=s2)
+    {
+        if(ranks[s2]>ranks[s1])
+        {
+            parent[s1]=s2;
+            ranks[s2]+=ranks[s1];
+        }
+        else
+        {
+            parent[s2]=s1;
+            ranks[s1]+=ranks[s2];
+        }
+    }
+}
+
+
+
+int32_t main()
+{
+int n,m;
+cin>>n>>m;
+parent.resize(n,-1);
+ranks.resize(n,1);
+for(int i=0;i<m;i++)
+{
+    int NuOfPeople;
+    cin>>NuOfPeople;
+    VI friends(NuOfPeople);
+    for(int i=0;i<NuOfPeople;i++)
+    {
+        int a;
+        cin>>a;
+        a--;
+        friends[i]=a;
+
+    }
+    for (int i=0;i<NuOfPeople-1;i++)
+    {
+        int a=friends[i];
+        int b=friends[i+1];
+        union_set(a,b);
+    }
+    
+}
+for(int i=0;i<n;i++)
+{
+    cout<<ranks[Find(i)]<<" ";      
+}
+
+return 0;
+}
