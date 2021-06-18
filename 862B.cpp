5524a9525f348673abff8f2ae47e8f60efaef966
parul-jain5102 <63896325+parul-jain5102@users.noreply.github.com>
2021-06-18 21:27:36 +0530

    Create 862B.cpp

---
+++ 862B.cpp
@@ -0,0 +1,85 @@
+// Problem 862B codeforces 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define MP make pair
+#define PB push back
+#define MOD 1000000007
+#define fi first #define se second
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<PII> VPII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSET;
+const int N=2e5+5;
+VI adj[N];
+VI vis(N,-1);
+int n;
+set<int> s1;
+set<int> s2;
+
+void BFS()
+{
+    queue<pair<int,int>> q; //node and its color
+    s1.insert(1);
+    q.push({1,1});
+    vis[1]=1;
+    while(!q.empty())
+    {
+        auto u=q.front();
+        q.pop();
+        int CurrVertex=u.first;
+        int CurrColor=u.second;
+        for(auto x:adj[CurrVertex])
+        {
+            if(vis[x]==-1)
+            {   vis[x]=3-CurrColor; //actually the color of x (if it was 1    earlier,   make it 3-1=2 and if it was 2 earlier ,make it 3-2=1 )
+              if(vis[x]==1)
+                  s1.insert(x);
+              if(vis[x]==2)
+                  s2.insert(x);
+
+             q.push({x,vis[x]});
+            }
+            else if(vis[x]==CurrColor)
+            {
+                // cout<<"Vertex "<<CurrVertex<<" and "<<x<<" have same color "<<CurrColor<<endl;
+                return;
+            }
+        }
+
+
+    }
+    // cout<<"The graph is Bi-Partite"<<endl;
+
+
+}
+void BiPartite()
+{
+    BFS();
+
+}
+
+
+int32_t main()
+{
+cin>>n;
+for(int i=0;i<n-1;i++)
+{
+    int u,v;
+    cin>>u>>v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+BiPartite();
+int size1=s1.size();
+int size2=s2.size();
+int ans=(size1*size2)-(n-1);
+cout<<ans<<endl;
+
+
+  
+   return 0;
+}
